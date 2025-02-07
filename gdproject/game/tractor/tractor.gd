b8ba07135045e4145a9aebcd5d8208f46638d0db
Konstanty <kostu96@gmail.com>
2025-02-07 12:21:55 +0100

    WIP

--- gdproject/game/tractor/tractor.gd
+++ gdproject/game/tractor/tractor.gd
@@ -1,9 +1,5 @@
 extends RigidBody3D
 
-const SUSPENSION_REST_DISTANCE : float = 0.35
-const SUSPENSION_SPRING_STRENGTH : float = 10.0
-const SUSPENSION_SPRING_DAMPER : float = 2.0
-
 
 func _physics_process(delta: float) -> void:
 	pass
--- gdproject/game/tractor/wheel.gd
+++ gdproject/game/tractor/wheel.gd
@@ -1,6 +1,9 @@
 extends RayCast3D
 
 @export var wheel_radius : float = 0.5
+const SUSPENSION_REST_DISTANCE : float = 0.5
+const SUSPENSION_SPRING_STRENGTH : float = 30.0
+const SUSPENSION_SPRING_DAMPER : float = 3.0
 
 @onready var vehicle : RigidBody3D = get_parent()
 
@@ -10,10 +13,10 @@ func _physics_process(delta: float) -> void:
 	if is_colliding():
 		var raycast_collision_point = get_collision_point()
 		var distance = raycast_collision_point.distance_to(global_position)
-		var spring_length = clamp(distance - wheel_radius, 0, vehicle.SUSPENSION_REST_DISTANCE)
-		var spring_force = vehicle.SUSPENSION_SPRING_STRENGTH * (vehicle.SUSPENSION_REST_DISTANCE - spring_length)
+		var spring_length = clamp(distance - wheel_radius, 0, SUSPENSION_REST_DISTANCE)
+		var spring_force = SUSPENSION_SPRING_STRENGTH * (SUSPENSION_REST_DISTANCE - spring_length)
 		var spring_velocity = (prev_spring_length - spring_length) / delta
-		var damper_force = vehicle.SUSPENSION_SPRING_DAMPER * spring_velocity
+		var damper_force = SUSPENSION_SPRING_DAMPER * spring_velocity
 		var suspension_force = basis.y * (damper_force + spring_force)
 		prev_spring_length = spring_length
 		var point = Vector3(raycast_collision_point.x, raycast_collision_point.y + wheel_radius, raycast_collision_point.z)
