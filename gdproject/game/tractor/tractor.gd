f6ccba89e4d4105a43b677b659b7f6bad86a9173
Konstanty <kostu96@gmail.com>
2025-02-11 18:53:15 +0100

    WIP on car

--- gdproject/game/tractor/tractor.gd
+++
@@ -1,6 +0,0 @@
-extends RigidBody3D
-
-
-func _physics_process(delta: float) -> void:
-	pass
-	#steering = move_toward(steering, Input.get_axis("Right", "Left"), delta * 2)
--- gdproject/game/tractor/wheel.gd
+++ gdproject/game/tractor/wheel.gd
@@ -1,23 +1,29 @@
 extends RayCast3D
 
 @export var wheel_radius : float = 0.5
-const SUSPENSION_REST_DISTANCE : float = 0.5
-const SUSPENSION_SPRING_STRENGTH : float = 30.0
-const SUSPENSION_SPRING_DAMPER : float = 3.0
+@export var min_spring_length : float = 0.3
+@export var max_spring_length : float = 1.0
+@export var spring_rest_length : float = 0.8
+
+const SPRING_STRENGTH : float = 10.0
+const SUSPENSION_SPRING_DAMPER : float = 5.0
 
 @onready var vehicle : RigidBody3D = get_parent()
 
 var prev_spring_length : float = 0.0
+var prev_offset : float = 0.0
+
+func _ready() -> void:
+	# Set RayCast length to maximum distance the tire can touch the ground
+	set_target_position(Vector3(0.0, -(max_spring_length + wheel_radius), 0.0))
 
 func _physics_process(delta: float) -> void:
 	if is_colliding():
-		var raycast_collision_point = get_collision_point()
-		var distance = raycast_collision_point.distance_to(global_position)
-		var spring_length = clamp(distance - wheel_radius, 0, SUSPENSION_REST_DISTANCE)
-		var spring_force = SUSPENSION_SPRING_STRENGTH * (SUSPENSION_REST_DISTANCE - spring_length)
-		var spring_velocity = (prev_spring_length - spring_length) / delta
-		var damper_force = SUSPENSION_SPRING_DAMPER * spring_velocity
-		var suspension_force = basis.y * (damper_force + spring_force)
-		prev_spring_length = spring_length
-		var point = Vector3(raycast_collision_point.x, raycast_collision_point.y + wheel_radius, raycast_collision_point.z)
-		vehicle.apply_force(global_basis.y * suspension_force, point - vehicle.global_position)
+		pass
+	#	var raycast_origin : Vector3 = global_position
+	#	var raycast_destination : Vector3 = get_collision_point()
+	#	var distance_to_hit : float = raycast_destination.distance_to(raycast_origin)
+	#	
+	#	var spring_offset : float = REST_DISTANCE - distance_to_hit - wheel_radius
+	#	print(spring_offset)
+		#DebugDraw3D.draw_arrow(global_position, to_global(position + Vector3(-position.x, (suspension_force.y / 4), -position.z)), Color.GREEN, 0.01, true)
